3738bffa92abf840588ad0e60d21a1fd4321fe6e
lawrence <lawrencechh.cs@gmail.com>
2023-08-12 17:44:20 +0000

    server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/build_test'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/build_test/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
